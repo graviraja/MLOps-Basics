986ef1176ebb91fa8795725d51a36426e4b0f99f
ravirajag <raviraja@aavaz.ai>
2021-07-19 02:22:50 +0530

    using eval

--- week_6_github_actions/parse_json.py
+++ week_6_github_actions/parse_json.py
@@ -4,7 +4,9 @@ with open('creds.txt') as f:
 	data = f.read()
 
 print(data)
-data = json.loads(data, strict=False)
+# data = json.loads(data, strict=False)
+# print(data)
+data = eval(data)
 print(data)
 
 with open('test.json', 'w') as f:
