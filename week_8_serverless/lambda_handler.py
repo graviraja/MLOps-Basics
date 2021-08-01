94749d2ff649fb4e47936c33721bcd36c3a40b17
ravirajag <raviraja@aavaz.ai>
2021-08-01 21:56:56 +0530

    updated base image in lambda

--- week_8_serverless/lambda_handler.py
+++ week_8_serverless/lambda_handler.py
@@ -6,11 +6,9 @@ from inference_onnx import ColaONNXPredictor
 
 inferencing_instance = ColaONNXPredictor("./models/model.onnx")
 
-
 def lambda_handler(event, context):
 	"""
 	Lambda function handler for predicting linguistic acceptability of the given sentence
 	"""
 	print(event)
 	return inferencing_instance.predict(event["sentence"])
-
