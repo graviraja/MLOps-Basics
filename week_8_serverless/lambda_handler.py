8fd4b2d752a11b15cfdc59a324ccaa12d54434ce
ravirajag <raviraja@aavaz.ai>
2021-08-02 00:24:31 +0530

    added request parsing in lambda handler

--- week_8_serverless/lambda_handler.py
+++ week_8_serverless/lambda_handler.py
@@ -2,6 +2,7 @@
 Lambda wrapper
 """
 
+import json
 from inference_onnx import ColaONNXPredictor
 
 inferencing_instance = ColaONNXPredictor("./models/model.onnx")
@@ -11,8 +12,17 @@ def lambda_handler(event, context):
 	Lambda function handler for predicting linguistic acceptability of the given sentence
 	"""
 	print(event)
-	return inferencing_instance.predict(event["sentence"])
-
+	
+	if "resource" in event.keys():
+		http_method = event["httpMethod"]
+		if http_method == "GET":
+			sentence = event["queryStringParameters"]["sentence"]
+			return inferencing_instance.predict(sentence)
+		elif http_method == "POST":
+			body = json.loads(event["body"])
+			return inferencing_instance.predict(body["sentence"])
+	else:
+		return inferencing_instance.predict(event["sentence"])
 
 if __name__ == "__main__":
 	test = {"sentence": "this is a sample sentence"}
