38255ba16c4aaa1187b328ec391f222a2c32f635
ravirajag <raviraja@aavaz.ai>
2021-08-02 01:23:25 +0530

    updated workflow

--- week_8_serverless/lambda_handler.py
+++ week_8_serverless/lambda_handler.py
@@ -11,15 +11,11 @@ def lambda_handler(event, context):
 	"""
 	Lambda function handler for predicting linguistic acceptability of the given sentence
 	"""
-	print(event)
 	
 	if "resource" in event.keys():
-		http_method = event["httpMethod"]
 		body = event["body"]
 		body = json.loads(body)
-		print(event)
-		print(body)
-		print(body["sentence"])
+		print(f"Got the input: {body['sentence']}")
 		response = inferencing_instance.predict(body["sentence"])
 		return {
 			"statusCode": 200,
